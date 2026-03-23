dce9437bac80d6b0dcc75d0b311bbb763aff5011
Whit3WolfMast3r <davidevansortiz@gmail.com>
2026-03-22 17:40:18 -0700

    increase image allotment to 10mb fixed config file

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverActions: {
-    bodySizeLimit: '10mb', // Allows up to 10MB headshots
-  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  } as any, // 'as any' stops TypeScript from blocking the build if types are lagging
 };
 
 export default nextConfig;
